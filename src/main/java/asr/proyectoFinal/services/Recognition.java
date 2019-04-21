e44b9a8f7b213bc056abfec4b3e8b8c727d2b0f0
Alvarobartolo7 <33809605+Alvarobartolo7@users.noreply.github.com>
2019-04-21 19:25:38 +0200

    Commit reconocimiento visual

--- src/main/java/asr/proyectoFinal/services/Recognition.java
+++ src/main/java/asr/proyectoFinal/services/Recognition.java
@@ -17,33 +17,45 @@ public class Recognition
 	public static void visualRecognition()
 	{
 		IamOptions options = new IamOptions.Builder()
-				  .apiKey("8Q5h1hGZYnbDG-QGnaGSV1wX4fkJcCrFeNC9Zmi5OFGK")
-				  .build();
+				.apiKey("8Q5h1hGZYnbDG-QGnaGSV1wX4fkJcCrFeNC9Zmi5OFGK")
+				.build();
 
-				VisualRecognition service = new VisualRecognition("2019-04-09", options);
-				
-				File file;
+		VisualRecognition service = new VisualRecognition("2018-03-19",options);
+		service.setEndPoint("https://gateway.watsonplatform.net/visual-recognition/api");
+		
+		File file;
+		InputStream imagesStream = null;
+		FileInputStream fis = null;
 
-				try
-				{
-				file = new File("c:/Users/Álvaro/Documents/GitHub/ProyectoFinalCloud/src/main/java/asr/proyectoFinal/services/fruitbowl.jpg");
-				
-				System.out.println(file.exists());
-				  if (!file.exists()) 
-				  {
-					  file.createNewFile();
-				  }
-				  
-				InputStream imagesStream = new FileInputStream(file);
-				ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
-				  .imagesFile(imagesStream)
-				  .classifierIds(Arrays.asList("food"))
-				  .build();
-				ClassifiedImages result = service.classify(classifyOptions).execute();
-				System.out.println(result);
-				}
-				catch (IOException e) {
-					  e.printStackTrace();
-				}
+		try
+		{
+		//file = new File("c:/Users/Álvaro/Documents/GitHub/ProyectoFinalCloud/src/main/java/asr/proyectoFinal/services/fruitbowl.jpg");
+		file = new File("c:/Users/Álvaro/Documents/GitHub/ProyectoFinalCloud/src/main/java/asr/proyectoFinal/services/casa.jpg");
+		
+		System.out.println(file.exists());
+		  if (!file.exists()) 
+		  {
+			  file.createNewFile();
+		  }
+		  
+		fis = new FileInputStream(file);
+		imagesStream = fis;
+		
+		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+		  .imagesFile(imagesStream)
+		  .imagesFilename("imagen")
+		  //.imagesFileContentType("food")
+		  //.url("aaa.com")
+		  //.threshold((float) 0.6)
+		  //.classifierIds(Arrays.asList("food"))
+		  .owners(Arrays.asList("IBM"))
+		  //.acceptLanguage("yes")
+		  .build();
+		ClassifiedImages result = service.classify(classifyOptions).execute();
+		System.out.println(result);
+		}
+		catch (IOException e) {
+			  e.printStackTrace();
+		}
 	}
 }
